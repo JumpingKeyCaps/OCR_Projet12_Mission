38252214dd1af2f39eeb1310d8d43a2bd3609d3b
Samuel Lebailly <sam.meusa@gmail.com>
2024-09-30 19:17:59 +0200

    feat(MainActivity): Implement SplashScreen feature (install) + setup a custom exit animation transition for the splashscreen.

--- app/src/main/java/com/openclassroom/joiefull/MainActivity.kt
+++ app/src/main/java/com/openclassroom/joiefull/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.openclassroom.joiefull
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +14,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.openclassroom.joiefull.ui.theme.JoiefullTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Install splash screen (keep before the call of super.onCreate())
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        //TODO SPLASH SCREEN ----------
+        initSplashScreenEndAnimation()
+
+
+
+
+
+
+
+
+//TODO COMPOSITION ----------
+
         enableEdgeToEdge()
         setContent {
             JoiefullTheme {
@@ -27,8 +47,36 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+
+
     }
+
+
+
+    /**
+     * Method to setup the splash screen end transition animation.
+     */
+    private fun initSplashScreenEndAnimation(){
+        //Callback that is called when the splash screen is animating to the main content.
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            // Create your custom animation to execute a the end of the splashscreen.
+            val slideLeft = ObjectAnimator.ofFloat(splashScreenView,View.TRANSLATION_X,0f,-splashScreenView.width.toFloat())
+            slideLeft.interpolator = AccelerateInterpolator()
+            slideLeft.duration = 180L
+            slideLeft.doOnEnd { splashScreenView.remove() }
+            slideLeft.start()
+        }
+    }
+
+
+
+
 }
+
+
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
