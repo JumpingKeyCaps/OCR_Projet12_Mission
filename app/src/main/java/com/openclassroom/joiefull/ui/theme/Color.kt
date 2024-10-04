3eb3f6fd1731d3718d040b70a149c4fba1565c35
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-04 20:37:27 +0200

    feat(ui.theme): Add new color values.

--- app/src/main/java/com/openclassroom/joiefull/ui/theme/Color.kt
+++ app/src/main/java/com/openclassroom/joiefull/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val WhitePure = Color(0xFFFFFFFF)
+val BlackPure = Color(0xFF000000)
+val YellowRating = Color(0xFFF99F43)
