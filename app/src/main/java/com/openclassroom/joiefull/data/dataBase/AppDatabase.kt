f50a4ed394d44857eb6a2c6b5bcca2c060888462
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-17 23:28:28 +0200

    feat(data/dataBase): Add the local app database with Room.

---
+++ app/src/main/java/com/openclassroom/joiefull/data/dataBase/AppDatabase.kt
@@ -0,0 +1,16 @@
+package com.openclassroom.joiefull.data.dataBase
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.openclassroom.joiefull.data.dataBase.dao.ProductDetailsDtoDao
+import com.openclassroom.joiefull.data.dataBase.entity.ProductDetailsDto
+
+/**
+ * The Database of the App ( with Room)
+ */
+@Database(entities = [ProductDetailsDto::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun productDetailsDao(): ProductDetailsDtoDao
+
+}
