7242aab3aceaaf0bac90b5cfbf720b70ac867142
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-17 23:05:43 +0200

    fix(conflict): Update the MainActivity and the product object.

--- app/src/main/java/com/openclassroom/joiefull/MainActivity.kt
+++ app/src/main/java/com/openclassroom/joiefull/MainActivity.kt
@@ -7,16 +7,15 @@ import android.view.animation.AccelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.openclassroom.joiefull.compositions.MainComposition
-import com.openclassroom.joiefull.model.Product
-import com.openclassroom.joiefull.ui.theme.JoiefullTheme
+import com.openclassroom.joiefull.compositions.JoiefullApp
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Main activity of the application.
  */
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     /**
@@ -26,35 +25,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         //Install splash screen (keep before the call of super.onCreate())
         installSplashScreen()
+        //Init splash screen end animation
+        initSplashScreenEndAnimation()
+        //Call super.onCreate()
         super.onCreate(savedInstanceState)
         //Edge to edge navigation mode feature
         enableEdgeToEdge()
-        //TODO SPLASH SCREEN ----------
-        initSplashScreenEndAnimation()
-
-
-
-        //Product List data sample (TO REMOVE)------------
-        val sampleProducts = listOf(
-            Product(id = 1,name = "Sac à main orange",price = "19.09€",originalPrice = "24.99€",likes = 12,rating = 4.5f, imageDescription = "", category = "Accessories",imageUrl = "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/main/img/accessories/1.jpg"),
-            Product(id = 2,name = "Jean pour femme",price = "32.29€",originalPrice = "34.99€",likes = 25,rating = 2.7f,imageDescription = "", category = "Accessories",imageUrl = "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/main/img/bottoms/1.jpg"),
-            Product(id = 3,name = "Bottes noires pour l'automne",price = "54.49€",originalPrice = "74.99€",likes = 67,rating = 3.9f,imageDescription = "", category = "Accessories",imageUrl = "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/main/img/shoes/1.jpg"),
-            Product(id = 4,name = "Blazer marron",price = "9.99€",originalPrice = "29.99€",likes = 89,rating = 4.8f,imageDescription = "", category = "Accessories",imageUrl = "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/main/img/tops/1.jpg"),
-            Product(id = 5,name = "Pull vert femme",price = "27.59€",originalPrice = "39.99€",likes = 2,rating = 3.1f,imageDescription = "", category = "Accessories",imageUrl = "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/main/img/tops/2.jpg"),
-        )
-    //---------------------------------
-
-
-        //TODO COMPOSITION ----------
+        //Main content of the activity
         setContent {
-            JoiefullTheme {
-                Surface{
-                   MainComposition(sampleProducts)
-                }
-
-            }
+            JoiefullApp()
         }
-
     }
 
 
