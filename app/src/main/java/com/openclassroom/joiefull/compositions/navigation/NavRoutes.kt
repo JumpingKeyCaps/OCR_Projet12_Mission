e7e365c8b949ffc86544b8bde220c082210c928d
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-18 00:34:12 +0200

    feat(compositions/navigation): Add the navigation Routes of the app.

---
+++ app/src/main/java/com/openclassroom/joiefull/compositions/navigation/NavRoutes.kt
@@ -0,0 +1,11 @@
+package com.openclassroom.joiefull.compositions.navigation
+
+/**
+ * Navigation routes of the application.
+ */
+object NavRoutes {
+
+    const val PRODUCTS_SCREEN = "ProductsMainScreen"
+
+    const val PRODUCT_DETAIL_SCREEN = "ProductDetailScreen/{productId}"
+}
