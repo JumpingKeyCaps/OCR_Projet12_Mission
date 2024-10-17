21f77e117b5a009738d05285147b9c6bd7ff2a81
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-17 23:38:16 +0200

    feat(di): Add the AppConfig object to manage used server URL.

---
+++ app/src/main/java/com/openclassroom/joiefull/di/AppConfig.kt
@@ -0,0 +1,10 @@
+package com.openclassroom.joiefull.di
+/**
+ * Object App Config to easy use of all URL in app
+ */
+object AppConfig {
+
+        /** The server base URL for real device on local network.*/
+    const val BASE_URL = "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/main/api/"
+
+}
