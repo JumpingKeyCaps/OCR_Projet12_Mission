8ca7910585d6b2cfe80d43200fd6ad4e8996aa06
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-17 22:26:22 +0200

    feat(compositions/composables): Add a component for the like button.

--- app/src/main/java/com/openclassroom/joiefull/compositions/composables/ButtonLikeComposable.kt
+++ app/src/main/java/com/openclassroom/joiefull/compositions/composables/ButtonLikeComposable.kt
@@ -1,4 +1,4 @@
-package com.openclassroom.joiefull.compositions.Composables
+package com.openclassroom.joiefull.compositions.composables
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,12 +9,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,9 +34,19 @@ import androidx.compose.ui.unit.sp
  * @param onClick The action to perform when the button is clicked.
  */
 @Composable
-fun LikeButton(likes: Int,modifier: Modifier,onClick: () -> Unit) {
-    ElevatedButton(modifier = modifier.width(51.dp).height(27.dp).padding(1.dp),
-        onClick = { onClick()},
+fun LikeButton(likes: Int,modifier: Modifier,onClick: () -> Unit,isLiked: Boolean, sizing: Int) {
+
+    //#######################
+
+    val likedIcon =  if (isLiked) Icons.Filled.Favorite else Icons.Default.FavoriteBorder
+
+
+    //###########################
+
+    ElevatedButton(modifier = modifier,
+        onClick = {
+            onClick() // Call the action from the onClick parameter
+        },
         colors = androidx.compose.material3.ButtonDefaults.buttonColors(Color.White),
         contentPadding = PaddingValues(1.dp)
     ) {
@@ -38,13 +54,13 @@ fun LikeButton(likes: Int,modifier: Modifier,onClick: () -> Unit) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
-            Icon(modifier = Modifier.size(14.dp),
-                imageVector = Icons.Default.FavoriteBorder,
+            Icon(modifier = Modifier.size(sizing.dp),
+                imageVector = likedIcon, //change the liked icon to match his state
                 contentDescription = "Like",
                 tint = Color.Black
             )
-            Spacer(modifier = modifier.width(2.dp))
-            Text(text = likes.toString(), fontSize = 14.sp, color = Color.Black, style = MaterialTheme.typography.bodySmall)
+            Spacer(modifier = Modifier.width(2.dp))
+            Text(text = likes.toString(), fontSize = sizing.sp, color = Color.Black, style = MaterialTheme.typography.bodySmall)
         }
     }
 }
@@ -53,5 +69,5 @@ fun LikeButton(likes: Int,modifier: Modifier,onClick: () -> Unit) {
 @Preview(showSystemUi = true)
 @Composable
 fun LikeButtonPreview() {
-    LikeButton(40,Modifier,onClick = {})
+    LikeButton(40,Modifier,onClick = {},isLiked = false,sizing = 14)
 }
