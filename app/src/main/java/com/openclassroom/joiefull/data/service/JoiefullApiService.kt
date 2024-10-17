b5b8861698409c4e5cf4efed0118b8c296c228e9
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-17 23:24:04 +0200

    feat(data/service): Add the app api service.

---
+++ app/src/main/java/com/openclassroom/joiefull/data/service/JoiefullApiService.kt
@@ -0,0 +1,25 @@
+package com.openclassroom.joiefull.data.service
+
+import com.openclassroom.joiefull.data.service.network.interfaces.JoiefullNetworkService
+import com.openclassroom.joiefull.model.Product
+import javax.inject.Inject
+
+
+/**
+ * The app API service to communicate with the server.
+ */
+class JoiefullApiService @Inject constructor(private val joiefullNetworkService: JoiefullNetworkService) {
+
+
+    /**
+     * Methode to get the clothes list.
+     *
+     * @return  a clothes objects list.
+     */
+    suspend fun getClothes(): List<Product> {
+        return joiefullNetworkService.getClothes()
+    }
+
+
+
+}
