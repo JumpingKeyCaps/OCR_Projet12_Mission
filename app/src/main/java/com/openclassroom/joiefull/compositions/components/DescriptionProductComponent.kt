83a5c8c1af4153dcd6a3fd1b7293086a373a74db
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-21 15:46:23 +0200

    feat(compositions/components): Rename and move the DescriptionProduct component to the components package + Kdoc + code cleaning.

--- app/src/main/java/com/openclassroom/joiefull/compositions/components/DescriptionProductComponent.kt
+++ app/src/main/java/com/openclassroom/joiefull/compositions/components/DescriptionProductComponent.kt
@@ -1,4 +1,4 @@
-package com.openclassroom.joiefull.compositions.composables
+package com.openclassroom.joiefull.compositions.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -9,8 +9,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+/**
+ * A composable function that displays a product description.
+ *
+ * @param description The description of the product.
+ * @param modifier The modifier to be applied to the composable.
+ */
 @Composable
-fun ProductDescription(description: String,modifier: Modifier){
+fun DescriptionProduct(description: String, modifier: Modifier){
     Text(text = description,
         fontSize = 14.sp,
         fontWeight = MaterialTheme.typography.bodyLarge.fontWeight,
@@ -19,10 +25,12 @@ fun ProductDescription(description: String,modifier: Modifier){
     )
 }
 
-
+/**
+ * A preview of the DescriptionProduct composable.
+ */
 @Preview(showSystemUi = true)
 @Composable
-fun ProductDescriptionPreview() {
-    ProductDescription("Pull vert forêt à motif torsadé élégant, tricot finement travaillé avec manches bouffantes et col montant; doux et chaleureux",
+fun DescriptionProductPreview() {
+    DescriptionProduct("Pull vert forêt à motif torsadé élégant, tricot finement travaillé avec manches bouffantes et col montant; doux et chaleureux",
         Modifier.padding(20.dp,0.dp,20.dp,0.dp))
 }
