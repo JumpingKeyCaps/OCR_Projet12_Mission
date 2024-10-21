0f4524498ed561f5216089aed6bfc82f65d9e65f
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-21 15:51:09 +0200

    feat(compositions/components): Move the like button to the components package + Upgrade + Kdoc + code cleaning.

--- app/src/main/java/com/openclassroom/joiefull/compositions/components/LikeButtonComponent.kt
+++ app/src/main/java/com/openclassroom/joiefull/compositions/components/LikeButtonComponent.kt
@@ -1,11 +1,9 @@
-package com.openclassroom.joiefull.compositions.composables
+package com.openclassroom.joiefull.compositions.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -16,36 +14,30 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.openclassroom.joiefull.R
 
 
 /**
  * A composable function that displays a button to add a product to favorites.
+ *
+ * @param likes The number of likes of the product.
+ * @param modifier The modifier to be applied to the button.
  * @param onClick The action to perform when the button is clicked.
+ * @param isLiked A boolean value indicating whether the product is liked or not.
+ * @param sizing The size of the text and icon in the button.
  */
 @Composable
 fun LikeButton(likes: Int,modifier: Modifier,onClick: () -> Unit,isLiked: Boolean, sizing: Int) {
-
-    //#######################
-
-    val likedIcon =  if (isLiked) Icons.Filled.Favorite else Icons.Default.FavoriteBorder
-
-
-    //###########################
-
     ElevatedButton(modifier = modifier,
         onClick = {
-            onClick() // Call the action from the onClick parameter
+            onClick()
         },
         colors = androidx.compose.material3.ButtonDefaults.buttonColors(Color.White),
         contentPadding = PaddingValues(1.dp)
@@ -55,8 +47,8 @@ fun LikeButton(likes: Int,modifier: Modifier,onClick: () -> Unit,isLiked: Boolea
             horizontalArrangement = Arrangement.Center
         ) {
             Icon(modifier = Modifier.size(sizing.dp),
-                imageVector = likedIcon, //change the liked icon to match his state
-                contentDescription = "Like",
+                imageVector = if (isLiked) Icons.Filled.Favorite else Icons.Default.FavoriteBorder,
+                contentDescription = stringResource(R.string.like_button_content_description),
                 tint = Color.Black
             )
             Spacer(modifier = Modifier.width(2.dp))
@@ -65,7 +57,9 @@ fun LikeButton(likes: Int,modifier: Modifier,onClick: () -> Unit,isLiked: Boolea
     }
 }
 
-
+/**
+ * A preview of the LikeButton composable.
+ */
 @Preview(showSystemUi = true)
 @Composable
 fun LikeButtonPreview() {
