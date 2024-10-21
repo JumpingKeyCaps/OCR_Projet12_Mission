7c2645562305ccf47b4cca3c3e4669f96ed6d07a
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-21 16:27:05 +0200

    feat(data/dataBase): Add the code documentation + code cleaning.

--- app/src/main/java/com/openclassroom/joiefull/data/dataBase/AppDatabase.kt
+++ app/src/main/java/com/openclassroom/joiefull/data/dataBase/AppDatabase.kt
@@ -6,11 +6,9 @@ import com.openclassroom.joiefull.data.dataBase.dao.ProductDetailsDtoDao
 import com.openclassroom.joiefull.data.dataBase.entity.ProductDetailsDto
 
 /**
- * The Database of the App ( with Room)
+ * The Database of the App (with Room)
  */
 @Database(entities = [ProductDetailsDto::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
-
     abstract fun productDetailsDao(): ProductDetailsDtoDao
-
 }
