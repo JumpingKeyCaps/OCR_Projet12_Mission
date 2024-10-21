9e130f2c982a3067b49f14374abdadfeb3bd85d9
Samuel Lebailly <sam.meusa@gmail.com>
2024-10-21 16:06:04 +0200

    feat(compositions/components): Add an new version of the rating bar component + kdoc.

--- app/src/main/java/com/openclassroom/joiefull/compositions/components/RatingStarsBarComponent.kt
+++ app/src/main/java/com/openclassroom/joiefull/compositions/components/RatingStarsBarComponent.kt
@@ -1,9 +1,7 @@
-package com.openclassroom.joiefull.compositions.composables
+package com.openclassroom.joiefull.compositions.components
 
 import android.view.MotionEvent
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -23,24 +20,35 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.openclassroom.joiefull.R
-import com.openclassroom.joiefull.model.ProductDetails
-import com.openclassroom.joiefull.viewmodel.ProductDetailsViewModel
+import com.openclassroom.joiefull.ui.theme.RatingBarSelectedColor
+import com.openclassroom.joiefull.ui.theme.RatingBarUnselectedColor
 
+/**
+ * A composable function that displays a rating bar with 5 stars.
+ *
+ * @param modifier The modifier to be applied to the composable.
+ * @param size The size of the stars.
+ * @param ratingIconPainter The painter for the rating icon.
+ * @param selectedColor The color of the selected star.
+ * @param unselectedColor The color of the unselected star.
+ * @param ratingState The state of the rating.
+ * @param onRatingChanged The callback function when the rating is changed.
+ */
 @Composable
-fun RatingBar2(
+fun RatingStarsBar(
     modifier: Modifier = Modifier,
     size: Dp = 34.dp,
     ratingIconPainter: Painter = painterResource(id = R.drawable.rounded_kid_star_24),
-    selectedColor: Color = Color(0xFFFFD700),
-    unselectedColor: Color = Color(0xFFA2ADB1),
+    selectedColor: Color = RatingBarSelectedColor,
+    unselectedColor: Color = RatingBarUnselectedColor,
     ratingState: MutableState<Float>,
     onRatingChanged: (Float) -> Unit
 ) {
-
     Row(modifier = modifier.wrapContentSize()) {
         // Star Icon Generation Loop ( rating on 5 stars)
         for (value in 1..5) {
@@ -55,10 +63,20 @@ fun RatingBar2(
             )
         }
     }
-
-
 }
 
+
+/**
+ * A composable function that displays a touchable star icon.
+ *
+ * @param size The size of the star icon.
+ * @param painter The painter for the star icon.
+ * @param ratingValue The value of the rating.
+ * @param selectedColor The color of the selected star.
+ * @param unselectedColor The color of the unselected star.
+ * @param ratingState The state of the rating.
+ * @param onRatingChanged The callback function when the rating is changed.
+ */
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun StarIcon(
@@ -72,13 +90,13 @@ fun StarIcon(
 ) {
     val tint by animateColorAsState(
         targetValue = if (ratingValue <= ratingState.value) selectedColor else unselectedColor,
-        label = ""
+        label = stringResource(id = R.string.star_icon_tint_animation_label)
     )
     Icon(
         painter = painter,
         contentDescription = null,
         modifier = Modifier
-            .padding(10.dp,0.dp,0.dp,0.dp)
+            .padding(10.dp, 0.dp, 0.dp, 0.dp)
             .size(size)
             .pointerInteropFilter {
                 when (it.action) {
@@ -93,16 +111,20 @@ fun StarIcon(
 }
 
 
+
+/**
+ * A preview of the RatingStarsBar composable.
+ */
 @Preview(showSystemUi = true)
 @Composable
-fun RatingBar2Preview() {
+fun RatingStarsBarPreview() {
     val ratingState = remember { mutableFloatStateOf(0.0f) }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-      //  RatingBar2(ratingState = ratingState)
+    Row( modifier = Modifier.fillMaxSize()) {
+        RatingStarsBar(modifier = Modifier
+            .padding(10.dp, 0.dp, 0.dp, 0.dp)
+            .align(Alignment.CenterVertically),
+            ratingState = ratingState,
+            onRatingChanged = {})
     }
 }
 
